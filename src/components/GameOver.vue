45515a436292c7f1bbd44660751972c5769c4039
atomflunder <80397293+atomflunder@users.noreply.github.com>
2023-03-18 20:22:27 +0100

    Remove outdated comment

--- src/components/GameOver.vue
+++ src/components/GameOver.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-// TODO: Make this a modal, with the option to reset the game.
-</script>
 <template>
     <div class="game-over">GAME OVER!</div>
     <div class="game-over info">ENTER TO RESET</div>
