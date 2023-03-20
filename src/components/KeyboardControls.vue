11e76d8606465f67530e8f1c0757f842b5b990c4
atomflunder <80397293+atomflunder@users.noreply.github.com>
2023-03-20 16:26:03 +0100

    Use grid for keyboard controls

--- src/components/KeyboardControls.vue
+++ src/components/KeyboardControls.vue
@@ -24,40 +24,92 @@ function keyToDisplay(key: string): string {
 </script>
 
 <template>
-    CONTROLS:
-    <tr>
-        &nbsp;
-    </tr>
-    <table class="keyboard-controls">
-        <tr>
-            <td>MOVE LEFT / RIGHT:</td>
-            <td>
-                {{ keyToDisplay(CONTROLS.MOVE_LEFT) }} / {{ keyToDisplay(CONTROLS.MOVE_RIGHT) }}
-            </td>
-        </tr>
-        <tr>
-            <td>SOFT / HARD DROP:</td>
-            <td>{{ keyToDisplay(CONTROLS.SOFT_DROP) }} / {{ keyToDisplay(CONTROLS.HARD_DROP) }}</td>
-        </tr>
-        <tr>
-            <td>ROTATE CW / CCW:</td>
-            <td>
-                {{ keyToDisplay(CONTROLS.ROTATE_CW) }} / {{ CONTROLS.ROTATE_CCW.toUpperCase() }}
-            </td>
-        </tr>
-        <tr>
-            <td>HOLD PIECE:</td>
-            <td>{{ CONTROLS.HOLD_PIECE.toUpperCase() }}</td>
-        </tr>
-        <tr>
-            <td>INSERT GARBAGE:</td>
-            <td>{{ CONTROLS.INSERT_GARBAGE.toUpperCase() }}</td>
-        </tr>
-    </table>
+    <div class="keyboard-controls">
+        <div class="header">CONTROLS:</div>
+
+        <div class="label-move">MOVE LEFT / RIGHT:</div>
+        <div class="button-move">
+            {{ keyToDisplay(CONTROLS.MOVE_LEFT) }} / {{ keyToDisplay(CONTROLS.MOVE_RIGHT) }}
+        </div>
+
+        <div class="label-drop">SOFT / HARD DROP:</div>
+        <div class="button-drop">
+            {{ keyToDisplay(CONTROLS.SOFT_DROP) }} / {{ keyToDisplay(CONTROLS.HARD_DROP) }}
+        </div>
+
+        <div class="label-rotate">ROTATE CW / CCW:</div>
+        <div class="button-rotate">
+            {{ keyToDisplay(CONTROLS.ROTATE_CW) }} / {{ CONTROLS.ROTATE_CCW.toUpperCase() }}
+        </div>
+
+        <div class="label-hold">HOLD PIECE:</div>
+        <div class="button-hold">{{ CONTROLS.HOLD_PIECE.toUpperCase() }}</div>
+
+        <div class="label-garbage">INSERT GARBAGE:</div>
+        <div class="button-garbage">{{ CONTROLS.INSERT_GARBAGE.toUpperCase() }}</div>
+    </div>
 </template>
 
 <style scoped>
 .keyboard-controls {
-    border-spacing: 10px 30px;
+    display: grid;
+    justify-content: center;
+    gap: 1rem 1rem;
+}
+
+.header {
+    grid-column-start: 1;
+    grid-column-end: 3;
+    grid-row-start: 1;
+}
+
+.label-move {
+    grid-column-start: 1;
+    grid-row-start: 2;
+}
+
+.button-move {
+    grid-column-start: 2;
+    grid-row-start: 2;
+}
+
+.label-drop {
+    grid-column-start: 1;
+    grid-row-start: 3;
+}
+
+.button-drop {
+    grid-column-start: 2;
+    grid-row-start: 3;
+}
+
+.label-rotate {
+    grid-column-start: 1;
+    grid-row-start: 4;
+}
+
+.button-rotate {
+    grid-column-start: 2;
+    grid-row-start: 4;
+}
+
+.label-hold {
+    grid-column-start: 1;
+    grid-row-start: 5;
+}
+
+.button-hold {
+    grid-column-start: 2;
+    grid-row-start: 5;
+}
+
+.label-garbage {
+    grid-column-start: 1;
+    grid-row-start: 6;
+}
+
+.button-garbage {
+    grid-column-start: 2;
+    grid-row-start: 6;
 }
 </style>
